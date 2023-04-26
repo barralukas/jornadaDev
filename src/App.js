200d38124ad85551d03b78924299e6bf6d2267f9
Lucas <lucasna256@gmail.com>
2023-04-26 10:06:11 -0300

    aula 04

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video />
+        <Video 
+          likes={300}
+          messages={200}
+          shares={100}
+          name='Lucas'
+          description='Gato branco'
+          music='musica de tapas'
+          url='https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d'
+        />
+        <Video 
+          likes={300}
+          messages={200}
+          shares={100}
+        />
       </div>
     </div>
   );
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -2,8 +2,9 @@ import React, { useRef, useState } from "react";
 
 import "./video.css";
 import VideoFooter from "./components/footer/videoFooter";
+import VideoSidebar from "./components/sidebar/VideoSidebar";
 
-function Video() {
+function Video({ likes, messages, shares, name, description, music, url }) {
   const videoRef = useRef(null);
   const [play, setPlay] = useState(false);
 
@@ -24,9 +25,18 @@ function Video() {
         ref={videoRef}
         onClick={handleStart}
         loop
-        src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
+        src={url}
       ></video>
-      <VideoFooter />
+      <VideoSidebar 
+        likes={likes}
+        messages={messages}
+        shares={shares}
+      />
+      <VideoFooter
+        name={name}
+        description={description}
+        music={music}
+      />
     </div>
   );
 }
